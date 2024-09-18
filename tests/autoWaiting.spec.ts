1c9a3c399f8a2fdae34f8325f7cb11ab6a2a0153
Ala Willam <123258645+alawllm@users.noreply.github.com>
2024-09-18 17:13:24 +0200

    timeout

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto("http://uitestingplayground.com/ajax");
   await page.getByText("Button triggering AJAX request").click();
+  // modify the default timeout
+  testInfo.setTimeout(testInfo.timeout + 2000)
 });
 
 test("auto waiting", async ({ page }) => {
@@ -15,21 +17,29 @@ test("auto waiting", async ({ page }) => {
   expect(text).toContain("Data loaded with AJAX get request.");
 });
 
-test("alternative waits", async ({ page }) => {
+// test("alternative waits", async ({ page }) => {
+//   const successButton = page.locator(".bg-success");
+
+//   //wait for elment
+//   // await page.waitForSelector(".bg-success");
+
+//   //wait for particular response
+//   await page.waitForResponse("http://uitestingplayground.com/ajax");
+
+//   // wait for network calls to be completed - not recommended
+//   // await page.waitForLoadState("networkidle");
+
+//   // waiting for timeout - not recommended
+//   // await page.waitForTimeout(5000)
+
+//   const text = await successButton.allTextContents();
+//   expect(text).toContain("Data loaded with AJAX get request.");
+// });
+
+test("timeouts", async ({ page }) => {
   const successButton = page.locator(".bg-success");
-
-  //wait for elment
-  // await page.waitForSelector(".bg-success");
-
-  //wait for particular response
-  await page.waitForResponse("http://uitestingplayground.com/ajax");
-
-  // wait for network calls to be completed - not recommended
-  // await page.waitForLoadState("networkidle");
-
-  // waiting for timeout - not recommended
-  // await page.waitForTimeout(5000)
-
-  const text = await successButton.allTextContents();
-  expect(text).toContain("Data loaded with AJAX get request.");
+  // triple the default timeout
+  test.slow();
+  // override the default timeout
+  await successButton.click();
 });
