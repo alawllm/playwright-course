7b6115c9c15ee7135793d7206dabe2a2a2e7399c
Ala Willam <123258645+alawllm@users.noreply.github.com>
2024-11-08 10:56:55 +0100

    introduce docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,6 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   timeout: 40000,
-  globalTimeout: 60000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -73,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:4200/",
+    // reuseExistingServer: !process.env.CI,
+  },
 });
